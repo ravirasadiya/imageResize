ac3fddd06e95b8ee33a12bf23f5022b4f233bda8
ravirasadiya <ravirasadiya1994@gmail.com>
2020-05-05 21:06:44 +0530

    Initial Commit

---
+++ src/public/apidoc/api_data.js
@@ -0,0 +1,507 @@
+define({ "api": [
+  {
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "optional": false,
+            "field": "varname1",
+            "description": "<p>No type.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "varname2",
+            "description": "<p>With type.</p>"
+          }
+        ]
+      }
+    },
+    "type": "",
+    "url": "",
+    "version": "0.0.0",
+    "filename": "src/public/apidoc/main.js",
+    "group": "C__Users_Ravi_Downloads_s_api_src_public_apidoc_main_js",
+    "groupTitle": "C__Users_Ravi_Downloads_s_api_src_public_apidoc_main_js",
+    "name": ""
+  },
+  {
+    "type": "get",
+    "url": "/api/media/image-resize",
+    "title": "Resize Image On The Fly",
+    "group": "Resize_Image",
+    "parameter": {
+      "fields": {
+        "Request body": [
+          {
+            "group": "Request body",
+            "type": "String",
+            "optional": false,
+            "field": "width",
+            "description": "<p>width</p>"
+          },
+          {
+            "group": "Request body",
+            "type": "String",
+            "optional": false,
+            "field": "height",
+            "description": "<p>height</p>"
+          },
+          {
+            "group": "Request body",
+            "type": "String",
+            "optional": false,
+            "field": "name",
+            "description": "<p>name</p>"
+          },
+          {
+            "group": "Request body",
+            "type": "String",
+            "optional": false,
+            "field": "path",
+            "description": "<p>path</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "examples": [
+        {
+          "title": "Success",
+          "content": "HTTP/1.1 200 OK\n{\n  \"message\": \"Successfully resize image\",\n  \"status\": \"1\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "sampleRequest": [
+      {
+        "url": "/api/media/image-resize"
+      }
+    ],
+    "error": {
+      "examples": [
+        {
+          "title": "media error",
+          "content": "HTTP/1.1 500 Internal Server Error\n{\n    \"message\": \"Unable to resize the image\",\n    \"status\": 0,\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "src/api/controllers/MediaController.ts",
+    "groupTitle": "Resize_Image",
+    "name": "GetApiMediaImageResize"
+  },
+  {
+    "type": "get",
+    "url": "/api/media/bucket-object-list",
+    "title": "bucket-object-list",
+    "group": "media",
+    "header": {
+      "fields": {
+        "Header": [
+          {
+            "group": "Header",
+            "type": "String",
+            "optional": false,
+            "field": "Authorization",
+            "description": ""
+          }
+        ]
+      }
+    },
+    "parameter": {
+      "fields": {
+        "Request body": [
+          {
+            "group": "Request body",
+            "type": "Number",
+            "optional": false,
+            "field": "limit",
+            "description": "<p>list limit</p>"
+          },
+          {
+            "group": "Request body",
+            "type": "String",
+            "optional": false,
+            "field": "folderName",
+            "description": "<p>Specific Folder Name</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Input",
+          "content": "{\n     \"limit\" : \"\",\n     \"folderName\" : \"\",\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "success": {
+      "examples": [
+        {
+          "title": "Success",
+          "content": "HTTP/1.1 200 OK\n{\n     \"message\": \"Successfully get bucket object list!\",\n     \"status\": \"1\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "sampleRequest": [
+      {
+        "url": "/api/media/bucket-object-list"
+      }
+    ],
+    "error": {
+      "examples": [
+        {
+          "title": "media error",
+          "content": "HTTP/1.1 500 Internal Server Error",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "src/api/controllers/MediaController.ts",
+    "groupTitle": "media",
+    "name": "GetApiMediaBucketObjectList"
+  },
+  {
+    "type": "get",
+    "url": "/api/media/delete-file",
+    "title": "delete file API",
+    "group": "media",
+    "header": {
+      "fields": {
+        "Header": [
+          {
+            "group": "Header",
+            "type": "String",
+            "optional": false,
+            "field": "Authorization",
+            "description": ""
+          }
+        ]
+      }
+    },
+    "parameter": {
+      "fields": {
+        "Request body": [
+          {
+            "group": "Request body",
+            "type": "String",
+            "optional": false,
+            "field": "fileName",
+            "description": "<p>File Name</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Input",
+          "content": "{\n     \"fileName\" : \"\",\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "success": {
+      "examples": [
+        {
+          "title": "Success",
+          "content": "HTTP/1.1 200 OK\n{\n     \"message\": \"Successfully Deleted file!\",\n     \"status\": \"1\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "sampleRequest": [
+      {
+        "url": "/api/media/delete-file"
+      }
+    ],
+    "error": {
+      "examples": [
+        {
+          "title": "media error",
+          "content": "HTTP/1.1 500 Internal Server Error",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "src/api/controllers/MediaController.ts",
+    "groupTitle": "media",
+    "name": "GetApiMediaDeleteFile"
+  },
+  {
+    "type": "get",
+    "url": "/api/media/search-folder",
+    "title": "search Folder API",
+    "group": "media",
+    "header": {
+      "fields": {
+        "Header": [
+          {
+            "group": "Header",
+            "type": "String",
+            "optional": false,
+            "field": "Authorization",
+            "description": ""
+          }
+        ]
+      }
+    },
+    "parameter": {
+      "fields": {
+        "Request body": [
+          {
+            "group": "Request body",
+            "type": "String",
+            "optional": false,
+            "field": "folderName",
+            "description": "<p>folderName</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Input",
+          "content": "{\n     \"FolderName\" : \"\",\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "success": {
+      "examples": [
+        {
+          "title": "Success",
+          "content": "HTTP/1.1 200 OK\n{\n     \"message\": \"Successfully get Folder!\",\n     \"status\": \"1\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "sampleRequest": [
+      {
+        "url": "/api/media/search-folder"
+      }
+    ],
+    "error": {
+      "examples": [
+        {
+          "title": "media error",
+          "content": "HTTP/1.1 500 Internal Server Error",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "src/api/controllers/MediaController.ts",
+    "groupTitle": "media",
+    "name": "GetApiMediaSearchFolder"
+  },
+  {
+    "type": "post",
+    "url": "/api/media/create-folder",
+    "title": "Create Folder",
+    "group": "media",
+    "header": {
+      "fields": {
+        "Header": [
+          {
+            "group": "Header",
+            "type": "String",
+            "optional": false,
+            "field": "Authorization",
+            "description": ""
+          }
+        ]
+      }
+    },
+    "parameter": {
+      "fields": {
+        "Request body": [
+          {
+            "group": "Request body",
+            "type": "String",
+            "optional": false,
+            "field": "folderName",
+            "description": "<p>Specific Folder Name</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Input",
+          "content": "{\n     \"folderName\" : \"\",\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "success": {
+      "examples": [
+        {
+          "title": "Success",
+          "content": "HTTP/1.1 200 OK\n{\n     \"message\": \"Successfully Created folder!\",\n     \"status\": \"1\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "sampleRequest": [
+      {
+        "url": "/api/media/create-folder"
+      }
+    ],
+    "error": {
+      "examples": [
+        {
+          "title": "media error",
+          "content": "HTTP/1.1 500 Internal Server Error",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "src/api/controllers/MediaController.ts",
+    "groupTitle": "media",
+    "name": "PostApiMediaCreateFolder"
+  },
+  {
+    "type": "post",
+    "url": "/api/media/delete-folder",
+    "title": "delete folder API",
+    "group": "media",
+    "header": {
+      "fields": {
+        "Header": [
+          {
+            "group": "Header",
+            "type": "String",
+            "optional": false,
+            "field": "Authorization",
+            "description": ""
+          }
+        ]
+      }
+    },
+    "parameter": {
+      "fields": {
+        "Request body": [
+          {
+            "group": "Request body",
+            "type": "String",
+            "optional": false,
+            "field": "folderName",
+            "description": "<p>Specific Folder Name</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Input",
+          "content": "{\n     \"folderName\" : \"\",\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "success": {
+      "examples": [
+        {
+          "title": "Success",
+          "content": "HTTP/1.1 200 OK\n{\n     \"message\": \"Successfully Deleted folder!\",\n     \"status\": \"1\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "sampleRequest": [
+      {
+        "url": "/api/media/delete-folder"
+      }
+    ],
+    "error": {
+      "examples": [
+        {
+          "title": "media error",
+          "content": "HTTP/1.1 500 Internal Server Error",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "src/api/controllers/MediaController.ts",
+    "groupTitle": "media",
+    "name": "PostApiMediaDeleteFolder"
+  },
+  {
+    "type": "post",
+    "url": "/api/media/upload-file",
+    "title": "Upload File",
+    "group": "media",
+    "parameter": {
+      "fields": {
+        "Request body": [
+          {
+            "group": "Request body",
+            "type": "String",
+            "optional": false,
+            "field": "image",
+            "description": "<p>image</p>"
+          },
+          {
+            "group": "Request body",
+            "type": "String",
+            "optional": false,
+            "field": "path",
+            "description": "<p>Directory Name</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Input",
+          "content": "{\n  \"file\":\"\",\n  \"path\" : \"\",\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "header": {
+      "fields": {
+        "Header": [
+          {
+            "group": "Header",
+            "type": "String",
+            "optional": false,
+            "field": "Authorization",
+            "description": ""
+          }
+        ]
+      }
+    },
+    "success": {
+      "examples": [
+        {
+          "title": "Success",
+          "content": "HTTP/1.1 200 OK\n{\n  \"message\": \"Successfully upload file\",\n  \"status\": \"1\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "sampleRequest": [
+      {
+        "url": "/api/media/upload-file"
+      }
+    ],
+    "error": {
+      "examples": [
+        {
+          "title": "media error",
+          "content": "HTTP/1.1 500 Internal Server Error\n{\n    \"message\": \"Unable to upload file\",\n    \"status\": 0,\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "src/api/controllers/MediaController.ts",
+    "groupTitle": "media",
+    "name": "PostApiMediaUploadFile"
+  }
+] });
---
+++ src/public/apidoc/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "SpurtCommerce V3.0.0",
+  "version": "3.0.0",
+  "description": "SpurtCommerce RESTFUL API Document",
+  "title": "SpurtCommerce RESTFUL API Document",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-05-05T15:24:51.508Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
